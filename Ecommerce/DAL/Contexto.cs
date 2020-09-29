c1f4ae6e999b48e7979d6a04355d1623a8632db1
Mauricio Schiavo <mauricio@atendare.com>
2020-09-28 23:57:06 -0300

    New: First DB connected version

---
+++ Ecommerce/DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Proxies;
+
+namespace DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto() : base(Connection()) { }
+
+        private static DbContextOptions<Contexto> Connection()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<Contexto>();
+            optionsBuilder.UseLazyLoadingProxies();
+            optionsBuilder.UseMySQL("server=localhost;database=ecommercecti;user=cti;password=123456;port=3306");
+            return optionsBuilder.Options;
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ Ecommerce/Ecommerce/Controllers/ProdutoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            Contexto contexto = new Contexto();
+            var lsProdutos = contexto.Produtos.ToList();
+            return View(lsProdutos);
+        }
+    }
+}
