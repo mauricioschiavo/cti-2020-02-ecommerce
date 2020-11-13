f42c2b562ad318d6f3d7152c0feafa28f596af5a
Maur'icio <mauricioschiavo@gmail.com>
2020-11-13 18:48:53 -0300

    New: Create Account

---
+++ Ecommerce/Ecommerce/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new Customer());
+        }
+
+        [HttpPost]
+        public IActionResult Index(Customer customer)
+        {
+            return View(customer);
+        }
+    }
+}
